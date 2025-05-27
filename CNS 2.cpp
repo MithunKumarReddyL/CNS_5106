29c8cf0226cd6ae5efa6b1028a90be2cca1fea28
MithunKumarReddyL <166578052+MithunKumarReddyL@users.noreply.github.com>
2025-05-27 18:10:22 +0530

    Add files via upload

---
+++ CNS 2.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    char text[100], key[26] = "QWERTYUIOPASDFGHJKLZXCVBNM";
+    int i;
+
+    scanf("%s", text);
+
+    for (i = 0; text[i] != '\0'; i++) {
+        if (text[i] >= 'a' && text[i] <= 'z')
+            text[i] = key[text[i] - 'a'];
+        else if (text[i] >= 'A' && text[i] <= 'Z')
+            text[i] = key[text[i] - 'A'];
+    }
+
+    printf("%s\n", text);
+
+    return 0;
+}
+
